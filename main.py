d3b9ce40fc3a16d31b5ec3b226009ca1870b9401
dekim <ruslanzabroda951@gmail.com>
2024-11-18 19:08:59 +0300

    Добавили функцию calc

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 def print_hi(name):
     print(f'Hi, {name}')
     print('то изменение в файле')
+    # a71c74d09d6fa33c77f7aca2ec5ced0da8a1e69d
     print('ещечтотоменяем')
+
+
+# 89aa6fbdc48ce990813649aac449ab6b3839abb3
+
+def calc(a, b):
+    return a + b
 
 
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+    print(calc(10, 15))
